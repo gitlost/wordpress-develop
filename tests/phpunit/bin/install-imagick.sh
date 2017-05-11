c01b992d17b60351181643d3a6970d0b29066d4e
gitlost <burmartke@gmail.com>
2017-05-11 01:59:02 +0100

    Try ImageMagick 6.7.0-10 and all PHP versions.

---
+++ tests/phpunit/bin/install-imagick.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# ImageMagick version to use
+IMAGEMAGICK_VERSION='6.7.0-10'
+
+install_imagemagick() {
+	curl -O "https://www.imagemagick.org/download/releases/ImageMagick-$IMAGEMAGICK_VERSION.tar.gz" -f
+	tar xzf "ImageMagick-$IMAGEMAGICK_VERSION.tar.gz"
+	rm "ImageMagick-$IMAGEMAGICK_VERSION.tar.gz"
+	cd "ImageMagick-$IMAGEMAGICK_VERSION"
+
+	./configure --prefix="$HOME/opt/$TRAVIS_PHP_VERSION"
+	make
+	make install
+
+	cd "$TRAVIS_BUILD_DIR"
+}
+
+# Install ImageMagick if the current version isn't up to date
+PATH="$HOME/opt/$TRAVIS_PHP_VERSION/bin:$PATH" identify -version | grep "$IMAGEMAGICK_VERSION" || install_imagemagick
+
+# Debugging
+ls "$HOME/opt/$TRAVIS_PHP_VERSION"
+
+# Install Imagick for PHP
+echo "$HOME/opt/$TRAVIS_PHP_VERSION" | pecl install imagick
